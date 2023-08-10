29b3643e8c7760208a4ef7154fb9ea5176b622fa
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-08-10 12:45:02 +0600

    navbar and banner section created

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Navbar2 from "../Pages/Shared/Navbar/Navbar2";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar2></Navbar2>
+            <Outlet></Outlet>
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default Main;
